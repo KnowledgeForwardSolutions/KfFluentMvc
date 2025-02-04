cd58bae8650e1ae226013fae1c50c36babb9aba0
mharges <michael.harges@gmail.com>
2025-02-03 22:17:15 -0800

    Additional validation methods

--- src/KfFluentMvc.WinForms/ValidatingMvcModelBase.cs
+++ src/KfFluentMvc.WinForms/ValidatingMvcModelBase.cs
@@ -59,4 +59,73 @@ public abstract class ValidatingMvcModelBase : MvcModelBase, IValidatingMvcModel
 
       return true;
    }
+
+   /// <summary>
+   ///   Property validation method that checks if a property has a 
+   ///   <see langword="null"/> value.
+   /// </summary>
+   /// <typeparam name="T">
+   ///   The property type.
+   /// </typeparam>
+   /// <param name="value">
+   ///   The value to check.
+   /// </param>
+   /// <param name="errorMessage">
+   ///   Error message to display if the property is <see langword="null"/>.
+   /// </param>
+   /// <param name="propertyName">
+   ///   The name of the property being checked.
+   /// </param>
+   /// <returns>
+   ///   <see langword="true"/> if the property is valid (i.e. is not null); 
+   ///   otherwise <see langword="false"/>.
+   /// </returns>
+   protected virtual Boolean ValidatePropertyNotNull<T>(
+      T value,
+      String errorMessage,
+      [CallerArgumentExpression("value")] String propertyName = null!)
+   {
+      Errors.ClearPropertyErrors(propertyName);
+      if (value is null)
+      {
+         Errors.SetError(propertyName, errorMessage);
+         return false;
+      }
+
+      return true;
+   }
+
+   /// <summary>
+   ///   Property validation method that checks if a <see cref="String"/>
+   ///   property is <see langword="null"/>, <see cref="String.Empty"/> or all
+   ///   whitespace characters.
+   /// </summary>
+   /// <param name="value">
+   ///   The value to check.
+   /// </param>
+   /// <param name="errorMessage">
+   ///   Error message to display if the property is <see langword="null"/>.
+   /// </param>
+   /// <param name="propertyName">
+   ///   The name of the property being checked.
+   /// </param>
+   /// <returns>
+   ///   <see langword="true"/> if the property is valid (i.e. is not null,
+   ///   not String.Empty or all whitespace characters); otherwise 
+   ///   <see langword="false"/>.
+   /// </returns>
+   protected virtual Boolean ValidatePropertyNotNullOrWhiteSpace(
+      String value,
+      String errorMessage,
+      [CallerArgumentExpression("value")] String propertyName = null!)
+   {
+      Errors.ClearPropertyErrors(propertyName);
+      if (String.IsNullOrWhiteSpace(value))
+      {
+         Errors.SetError(propertyName, errorMessage);
+         return false;
+      }
+
+      return true;
+   }
 }
